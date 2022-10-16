8b28a706cb722006589d8446e776e776add4f44e
bassamke <mathekasimon@gmail.com>
2022-10-16 13:03:45 +0300

    getElementsByClassName

--- calc.js
+++ calc.js
@@ -1,4 +1,6 @@
 let display = document.getElementById('Display');
 
-let buttons =document.getElementByClassName('button');
-console.log('buttons');
+let buttons =document.getElementsByClassName('button'); //use getElementsByClassName not Element //was missing the s
+for(var i=0; i < buttons.length; i++) {  //getElementsByClassName returns an array of all buttons, you can loop through each item to get it's value
+    console.log(buttons[i].value);
+  }
